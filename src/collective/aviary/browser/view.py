412c8e3f0593b066dd924f94663c4ee2ff53a0be
Marcos F. Romero <marcosfromero@gmail.com>
2013-12-20 17:32:07 -0300

    Transform tab, with issues

--- src/collective/aviary/browser/view.py
+++ src/collective/aviary/browser/view.py
@@ -1,10 +1,53 @@
 # -*- coding: utf-8 -*-
 
-from five import grok
+from collective.aviary.interfaces import IAviarySettings
+from plone.registry.interfaces import IRegistry
+from Products.Five import BrowserView
+from zope.component import getUtility
 
-grok.templatedir('templates')
 
-
-class HelloWorld (grok.view):
-    """Browserview de exemplo Hello World
+class AviaryTransform(BrowserView):
+    """ Aviary Transform tab
     """
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def javascript(self):
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(IAviarySettings)
+
+        js_code = """
+var featherEditor = new Aviary.Feather({
+    apiKey: "{0}",
+    apiVersion: {1},
+    theme: "{2}",
+    tools: "{3}",
+    appendTo: "content",
+    onSave: function(imageID, newURL) {
+        $("#" + imageID).attr("src", newURL);
+    },
+    onError: function(errorObj) {
+        alert(errorObj.message);
+    }
+    });
+    function launchEditor(el) {
+        el = $(el);
+        // Image with no id, set a random id
+        if(!el.attr("id")) {
+            var id = String(Math.random()).split(".").pop();
+            el.attr("id", id);
+        }
+        id = el.attr("id");
+        src = el.attr("src");
+        featherEditor.launch({
+            image: id,
+            url: src
+        });
+        return false;
+    }
+"""
+
+        return js_code.format(settings.api_key, settings.api_version,
+                              settings.theme, settings.tools)
